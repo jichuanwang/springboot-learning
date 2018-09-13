30192253aa04853d77f9e61bc37cc91e179f7fe5
wangjichuan <wangjichuan2011@126.com>
2018-09-13 08:37:54 +0800

    mac 修改  netty学习

--- springboot-learning-web/src/test/java/com/dolphin/netty/TimeServer.java
+++ springboot-learning-web/src/test/java/com/dolphin/netty/TimeServer.java
@@ -8,6 +8,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
 import org.jboss.netty.channel.ChildChannelStateEvent;
 
 
@@ -31,7 +32,12 @@ public class TimeServer {
 
         @Override
         protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new StringDecoder());
             socketChannel.pipeline().addLast(new TimeServerHandler());
         }
+    }
+
+    public static void main(String[] args) throws Exception{
+        new TimeServer().bind(1110);
     }
 }

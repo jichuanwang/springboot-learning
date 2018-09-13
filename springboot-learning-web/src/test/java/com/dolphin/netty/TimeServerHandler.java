c80770f22b3bfb3374f3c3966b6b42348ca40e8c
wangjichuan <wangjichuan2011@126.com>
2018-09-13 08:38:12 +0800

    mac 修改  netty学习

--- springboot-learning-web/src/test/java/com/dolphin/netty/TimeServerHandler.java
+++ springboot-learning-web/src/test/java/com/dolphin/netty/TimeServerHandler.java
@@ -1,11 +1,15 @@
 package com.dolphin.netty;
 
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 
 /**
  * Created by wangjichuan on 2018/9/11.
  */
-public class TimeServerHandler extends ChannelHandlerAdapter {
-
-
+public class TimeServerHandler extends SimpleChannelInboundHandler {
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+        System.out.println(o);
+    }
 }
